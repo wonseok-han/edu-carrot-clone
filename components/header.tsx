13a5fadfbdcaaac65844fa720345d7847329d005
wonseok-han <oshan1112@gmail.com>
2023-06-03 16:42:17 +0900

    tailwind enter페이지 추가로 인한 구조변경

--- components/header.tsx
+++ components/header.tsx
@@ -3,9 +3,10 @@ import Menu from "./menu";
 
 const MENUS = [
   { text: "home", url: "/" },
-  { text: "tailwind", url: "pages/tailwind/" },
+  { text: "tailwind", url: "pages/tailwind/tour/" },
   { text: "form", url: "pages/form/" },
   { text: "details", url: "pages/details/" },
+  { text: "enter", url: "pages/tailwind/enter/" },
 ];
 
 const Header: NextPage = () => {
