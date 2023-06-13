4b228fccce97d02004b660896d63637975922f4a
wonseok-han <oshan1112@gmail.com>
2023-06-13 18:11:35 +0900

    prisma client 추가, 테스트 api 추가

---
+++ app/api/client-test/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import client from "@/libs/client";
+
+export async function GET() {
+  await client.user.create({
+    data: { email: "hi", name: "hi" },
+  });
+
+  return NextResponse.json({
+    ok: true,
+  });
+}
+
+// export async function POST(req: Request) {
+//   const data = {
+//     email: "hi",
+//     name: "hi",
+//   };
+
+//   client.user.create({
+//     data: data,
+//   });
+
+//   const res = await req.json();
+//   return NextResponse.json({ data });
+// }
