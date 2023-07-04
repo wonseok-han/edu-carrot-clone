3a4ab028195ab6aee1eebaf73b5f6ffe2cca868b
wonseok-han <oshan1112@gmail.com>
2023-07-04 13:38:51 +0900

    Input 컴포넌트 분리

--- app/pages/tailwind/enter/page.tsx
+++ app/pages/tailwind/enter/page.tsx
@@ -1,12 +1,32 @@
 "use client";
 
+import Input from "@/components/input";
 import { cls } from "@/libs/utils";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface EnterForm {
+  email?: string;
+  phone?: string;
+}
 
 export default function Enter() {
+  const { register, watch, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
-  const onEmailClick = () => setMethod("email");
-  const onPhoneClick = () => setMethod("phone");
+
+  const onEmailClick = () => {
+    reset();
+    setMethod("email");
+  };
+  const onPhoneClick = () => {
+    reset();
+    setMethod("phone");
+  };
+  const onValid = (data: EnterForm) => {
+    console.log(watch);
+    console.log(data);
+  };
+
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -38,34 +58,27 @@ export default function Enter() {
             </button>
           </div>
         </div>
-        <form className="flex flex-col mt-8">
-          <label htmlFor="input" className="text-sm font-medium text-gray-700">
-            {method === "email" ? "Email address" : null}
-            {method === "phone" ? "Phone number" : null}
-          </label>
-          <div className="mt-1">
-            {method === "email" ? (
-              <input
-                id="input"
-                type="email"
-                className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                required
-              />
-            ) : null}
-            {method === "phone" ? (
-              <div className="flex rounded-md shadow-sm">
-                <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none">
-                  +82
-                </span>
-                <input
-                  id="input"
-                  type="number"
-                  className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                  required
-                />
-              </div>
-            ) : null}
-          </div>
+        <form className="flex flex-col mt-8" onSubmit={handleSubmit(onValid)}>
+          {method === "email" ? (
+            <Input
+              label="Email address"
+              name="email"
+              kind="text"
+              type="email"
+              register={register("email")}
+              required
+            />
+          ) : null}
+          {method === "phone" ? (
+            <Input
+              label="Phone"
+              name="phone"
+              kind="text"
+              type="phone"
+              register={register("phone")}
+              required
+            />
+          ) : null}
           <button className="mt-6 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border-r-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
             {method === "email" ? "Get login link" : null}
             {method === "phone" ? "Get one-time password" : null}
@@ -75,13 +88,13 @@ export default function Enter() {
           <div className="relative">
             <div className="absolute w-full border-gray-300 border-t" />
             <div className="relative -top-3 text-center">
-              <span className="bg-slate-400 px-2 text-sm text-gray-500">
+              <span className="px-2 text-sm text-gray-500 bg-white">
                 Or enter with
               </span>
             </div>
           </div>
           <div className="grid grid-cols-2 gap-3 mt-6">
-            <button className="flex justify-center items-center py-2 px-4 border-gray-600 border-2 rounded-md shadow-sm bg-slate-400 text-sm font-medium text-gray-700 hover:bg-gray-50">
+            <button className="flex justify-center items-center py-2 px-4 border-gray-600 border-2 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
@@ -91,7 +104,7 @@ export default function Enter() {
                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </button>
-            <button className="flex justify-center items-center py-2 px-4 border-gray-600 border-2 rounded-md shadow-sm bg-slate-400 text-sm font-medium text-gray-700 hover:bg-gray-50">
+            <button className="flex justify-center items-center py-2 px-4 border-gray-600 border-2 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
